26bc63cb426353672c4fe0fbe8aa13e52f3c1878
rain <393743083@qq.com>
2017-04-11 22:20:34 +0800

    add json

--- src/main/java/com/searchui/web/MainController.java
+++ src/main/java/com/searchui/web/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
@@ -13,7 +14,7 @@ import java.util.Map;
  * Created by rain on 17-4-4.
  */
 
-@Controller
+@RestController
 class MainController {
 
     @RequestMapping(value="search",method = RequestMethod.GET)
@@ -30,5 +31,18 @@ class MainController {
         return mv;
 
     }
+
+    @RequestMapping(value = "get",method = RequestMethod.GET,produces = "application/json;charset=UTF-8")
+    public String getPages(
+            @RequestParam("keyword") String keyword,
+            @RequestParam("page") int page){
+
+        Query query = new Query();
+        Map<String,String> map = query.query(keyword,page);
+
+        return map.get("data");
+
+    }
+
 
 }
