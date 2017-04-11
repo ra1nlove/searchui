b31a4d5d1d2fee12ef093b1ffe1ea5e82d05c8b1
rain <393743083@qq.com>
2017-04-11 17:42:11 +0800

    add query

--- src/main/java/com/searchui/es/Query.java
+++ src/main/java/com/searchui/es/Query.java
@@ -1,5 +1,6 @@
 package com.searchui.es;
 
+import com.alibaba.fastjson.JSON;
 import org.apache.lucene.queryparser.flexible.core.builders.QueryBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
@@ -7,9 +8,13 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by rain on 17-4-4.
@@ -18,22 +23,73 @@ public class Query {
 
     TransportClient client ;
 
-    public Query() throws Exception{
-        client = new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.108.113.231"),9300));
+    public Query() {
+        try {
+            client = new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.108.113.231"), 9300));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
-    public void query(String keyword){
+    public String query(String keyword,int page){
+
+        List<Result> res = new ArrayList<Result>();
+
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.preTags("<em>");
+        highlightBuilder.postTags("</em>");
+        highlightBuilder.field("content");
+        highlightBuilder.field("title");
+        highlightBuilder.field("desc");
 
         SearchResponse response = client.prepareSearch("page").setTypes("fulltext")
-                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH).setQuery(QueryBuilders.termQuery("content",keyword))
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(QueryBuilders.multiMatchQuery(keyword,"title","desc","content","keywords"))
+                .highlighter(highlightBuilder)
+                .setSize(10)
+                .setFrom(page*10)
                 .get();
 
+        for(SearchHit hit : response.getHits()){
+            String title = null;
+            String desc = null;
+            String url;
 
+            if(hit.getHighlightFields().get("title")!=null){
+                title = hit.getHighlightFields().get("title").getFragments()[0].toString();
+            }else if(hit.getSource().get("title")!=null){
+                title = hit.getSource().get("title").toString();
+            }
+            if(title == null) continue;
+
+            if(hit.getHighlightFields().get("desc")!=null){
+                desc = hit.getHighlightFields().get("desc").getFragments()[0].toString();
+            }else if(hit.getSource().get("desc")!=null){
+                desc = hit.getSource().get("desc").toString();
+            }
+
+            if(desc ==null){
+                if(hit.getHighlightFields().get("content")!=null){
+                    desc = hit.getHighlightFields().get("content").getFragments()[0].toString();
+                }else if(hit.getSource().get("content")!=null){
+                    desc = hit.getSource().get("content").toString();
+                }
+            }
+
+            if(desc.length()>100){
+                desc = desc.substring(0,100);
+            }
+            url = hit.getSource().get("url").toString();
+
+            Result re = new Result(title,desc,url);
+            res.add(re);
+        }
+       return JSON.toJSONString(res);
     }
 
     public static void main(String[] args) throws Exception{
         Query query = new Query();
-        query.query("新浪");
+        System.out.print(query.query("全球热恋",5));
     }
 
 }
--- src/main/java/com/searchui/web/MainController.java
+++ src/main/java/com/searchui/web/MainController.java
@@ -1,8 +1,11 @@
 package com.searchui.web;
 
+import com.searchui.es.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by rain on 17-4-4.
@@ -11,9 +14,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 class MainController {
 
-    @RequestMapping(value="index",method = RequestMethod.GET)
-    public String index(){
-        return "index";
+    @RequestMapping(value="search",method = RequestMethod.GET)
+    public ModelAndView index(
+                @RequestParam("keyword") String keyword,
+                @RequestParam("page") int page){
+
+        Query query = new Query();
+        String pages = query.query(keyword,page);
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message",pages);
+        mv.setViewName("search");
+        return mv;
     }
 
 }
